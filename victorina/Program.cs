01a674a212e023917a5f95f6d8040c67e297f869
VasKuzn <113028860+VasKuzn@users.noreply.github.com>
2024-12-22 19:10:03 +0500

    добавлена викторина

---
+++ victorina/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Сколько стоит хлеб?");
+int sum = 0;
+int ans = int.Parse(Console.ReadLine());
+switch (ans)
+{
+    case 15:
+        sum += 10;
+        Console.WriteLine($"ответ верный, +10 очков! к счету. Текущий: {sum}");
+        break;
+    default:
+        Console.WriteLine($"ответ неверный Текущий счёт: {sum}");
+        break;
+}
+Console.WriteLine("Сколько стоит молоко?");
+string ans1 = Console.ReadLine();
+switch (ans1.ToLower().Trim())
+{
+    case "sp70":
+        sum += 10;
+        Console.WriteLine($"ответ верный, +10 очков! к счету. Текущий: {sum}");
+        break;
+    default:
+        Console.WriteLine($"ответ неверный Текущий счёт: {sum}");
+        break;
+}
+Console.WriteLine(sum);
